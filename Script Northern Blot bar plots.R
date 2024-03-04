652303721af8000b0f4484ba0dcf51e69b696aa9
aagnolin <alberto.agnolin.1@gmail.com>
2024-03-04 16:50:28 +0100

    Merged data and added boxplot for replicates

--- Script Northern Blot bar plots.R
+++ Script Northern Blot bar plots.R
@@ -1,10 +1,25 @@
 library(readxl)
+library(tidyverse)
 NB_quantification_2024_02_12 <- read_excel("C:/Users/aagnoli/OneDrive - UvA/Gels/Northern Blots/2024-02-12 Northern Blot/2024-02-12 Quantification Northern Blot ImageStudio.xlsx")
 NB_quantification_2024_02_20 <- read_excel("C:/Users/aagnoli/OneDrive - UvA/Gels/Northern Blots/2024-02-20 Northern Blot/2024-02-20 Quantification Northern Blot ImageStudio.xlsx")
 
-data_NB <- NB_quantification_2024_02_20 
+Merged_NB <- rbind(NB_quantification_2024_02_12, NB_quantification_2024_02_20)
 
-ggplot(data = data_NB,
+data_NB <- Merged_NB
+
+
+#boxplot 13 and 20 duplo
+ggplot(data = filter(Merged_NB, Name == "BAA013" | Name == "BAA020"),
+       mapping = aes(x = Name,
+                     y = Signal)) +
+  geom_boxplot(fill = "grey", width = 0.5, linewidth = 1) +
+  geom_point(size = 2) +
+  theme_bw()
+  
+
+
+#barplot 2nd experiment
+ggplot(data = filter(Merged_NB, `Image Name` == "0007487_02"),
        mapping = aes(x = Name,
                      y = Signal)) +
   geom_col(col = 'black', linewidth = 1, width = 0.5) +
