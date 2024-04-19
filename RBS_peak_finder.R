5aec06c241f23e3dabc46d44c7403eeaae973a1a
aagnolin <alberto.agnolin.1@gmail.com>
2024-04-19 13:32:17 +0200

    Added calculations for translation initiation and ORF translation ratios

--- RBS_peak_finder.R
+++ RBS_peak_finder.R
@@ -68,8 +68,8 @@ gene_info_df <- data.frame(
 
 # Identify target sequences based on position values. Takes strand directionality into consideration
 target_sequences <- gene_info_df %>%
-  mutate(target_start = ifelse(Strand == "+", StartPosition - 30, EndPosition - 2),  #Write the numbers of the positions to select the range for sequence extraction
-         target_end = ifelse(Strand == "+", StartPosition + 2, EndPosition + 30)) %>%
+  mutate(target_start = ifelse(Strand == "+", StartPosition - 15, EndPosition - 5),  #Write the numbers of the positions to select the range for sequence extraction
+         target_end = ifelse(Strand == "+", StartPosition + 5, EndPosition + 15)) %>%
   select(LocusTag, target_start, target_end)
 
 #------------------------------------------------
@@ -118,6 +118,11 @@ Normalized_212_8h_1_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA seq
 Normalized_212_8h_2_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/Results 14-02-23 Yaozu ppGpp and codon exchange analysis/Results 14-02-23 Yaozu ppGpp and codon exchange analysis/ppGpp/Check RBS peak/Normalized_212_8h_2_35_full.csv")
 Normalized_ppGpp_8h_1_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/Results 14-02-23 Yaozu ppGpp and codon exchange analysis/Results 14-02-23 Yaozu ppGpp and codon exchange analysis/ppGpp/Check RBS peak/Normalized_ppGpp_8h_1_35_full.csv")
 Normalized_ppGpp_8h_2_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/Results 14-02-23 Yaozu ppGpp and codon exchange analysis/Results 14-02-23 Yaozu ppGpp and codon exchange analysis/ppGpp/Check RBS peak/Normalized_ppGpp_8h_2_35_full.csv")
+#All 1st replicates normalized by total reads between them
+N_WT_8h_1_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/ppGpp translation initiation issue/Normalized_212_8h_1_35_full.csv")
+N_ppGpp_8h_1_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/ppGpp translation initiation issue/Normalized_ppGpp_8h_1_35_full.csv")
+N_WT_16h_1_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/ppGpp translation initiation issue/Normalized_16h-1_ribo_35_full.csv")
+N_ppGpp_16h_1_35_full <- read_csv("C:/Users/aagnoli/OneDrive - UvA/RNA sequencing data/ppGpp translation initiation issue/Normalized_ppGpp_16h_1_35_full.csv")
 
 #Mup data
 Normalized_filtered_2X_Spin_53_full_14 <- read_csv("C:/Users/aagnoli/OneDrive - UvA/mup RBS finder/Normalized_filtered_2X_Spin_53_full_14.csv")
@@ -126,13 +131,22 @@ Normalized_filtered_2X_Spin_Jun_53_full_12 <- read_csv("C:/Users/aagnoli/OneDriv
 Normalized_filtered_2X_Spin_Jun_53_full_11 <- read_csv("C:/Users/aagnoli/OneDrive - UvA/mup RBS finder/Normalized_filtered_2X_Spin_Jun_53_full_11.csv")
 
 #Choose target positions (works in the same way as Sequence Finder. Takes strand directionality into consideration
+#-------------
+###INITIATION
 target_sequences_PeakFinder <- gene_info_df %>%
-  mutate(target_start = ifelse(Strand == "+", StartPosition - 30, EndPosition + 30),  #Write the numbers of the positions to select the range for sequence extraction
-         target_end = ifelse(Strand == "+", StartPosition, EndPosition)) %>%
+  mutate(target_start = ifelse(Strand == "+", StartPosition - 15, EndPosition + 15),  #Write the numbers of the positions to select the range for sequence extraction (-15 +15)
+         target_end = ifelse(Strand == "+", StartPosition + 5, EndPosition + 5)) %>% #I checked the + and - signs and this looks correct (+5 +5)
   select(LocusTag, target_start, target_end)
 
+###ORF
+target_sequences_PeakFinder <- gene_info_df %>%
+  mutate(target_start = ifelse(Strand == "+", StartPosition + 5, EndPosition - 5),  #Write the numbers of the positions to select the range for sequence extraction (-15 +15)
+         target_end = ifelse(Strand == "+", EndPosition, StartPosition)) %>% #I checked the + and - signs and this looks correct (+5 +5)
+  select(LocusTag, target_start, target_end)
+#-------------
+
 #select the input dataset from the ones loaded
-input_data <- Normalized_212_8h_1_53_full %>% dplyr::rename(LocusTag = "locus_tag")
+input_data <- N_ppGpp_16h_1_35_full %>% dplyr::rename(LocusTag = "locus_tag")
 input_data <- merge(input_data, target_sequences_PeakFinder, by = "LocusTag", all = TRUE)
 
 #Sequences outside the ORFs do not have a locus tag assigned, but this is necessary if we want to analyse peaks in the upstream region of the ORFs
@@ -170,19 +184,34 @@ for (i in 1:nrow(input_data)) {
 input_data <- input_data %>% dplyr::rename(locus_tag = "LocusTag")
 #Filter highest peaks on target regions of each locus tag and calculate the position of those peaks relative to the 1st nt of the start codon
 #Filtered_input_data <- input_data %>% filter(locus_tag == "BSU13280" | locus_tag == "BSU13280" | locus_tag == "BSU37350" | locus_tag == "BSU28860" | locus_tag == "BSU36660" | locus_tag == "BSU36650" | locus_tag == "BSU03780" | locus_tag == "BSU14180" | locus_tag == "BSU18060" | locus_tag == "BSU28290" | locus_tag == "BSU08760", position >= target_start & position <= target_end)
-input_data_max <- group_by(input_data, locus_tag) %>% filter(position >= target_start & position <= target_end, Norm_count == max(Norm_count))
+input_data_max <- group_by(input_data, locus_tag) %>% filter(position >= target_start & position <= target_end)
 input_data_max <- mutate(input_data_max, relative_position_RBS_peak = target_end - position)
 
 
 
 #merge dataset with target sequences for motif search
-input_data_max <- merge(input_data_max, subseq_df, by = "locus_tag")
+#input_data_max <- merge(input_data_max, subseq_df, by = "locus_tag")
 
-#plot results
+#add gene length
+gene_info_df <- gene_info_df %>% dplyr::rename(locus_tag = "LocusTag")
+gene_info_df <- gene_info_df %>% mutate(gene_length = EndPosition - StartPosition)
+input_data_max <- merge(input_data_max, gene_info_df, by = "locus_tag", all = TRUE) %>% select(- "gene_length.x")
+
+#plot results (also to double check your selection range is good)
 ggplot(data = input_data_max,
        mapping = aes(x = relative_position_RBS_peak)) +
   geom_bar() +
-  scale_x_reverse()
+  scale_x_reverse(breaks = seq(0,55, by = 1)) + theme(axis.text.x = element_text(vjust = 0.5,
+    angle = 45))
+
+
+#sum Norm_count in target positions for each gene, then divide by the target sequence length
+df_Ribo_reads_target_1 <- input_data_max %>% group_by(locus_tag) %>% summarize(Sum_Norm_count_initiation = sum(Norm_count)) %>% mutate(Translation_initiation_Ratio = Sum_Norm_count_initiation/20)
+#after repeating the previous script with another set of target positions, use this
+df_Ribo_reads_target_2 <- input_data_max %>% group_by(locus_tag) %>% summarize(Sum_Norm_count_ORF = sum(Norm_count)) %>% merge(gene_info_df, by = "locus_tag") %>% mutate(ORF_translation_Ratio = Sum_Norm_count_ORF/(gene_length -5)) #change this, it should be divided by gene length -5 
+Merged_ratios <- merge(df_Ribo_reads_target_1, df_Ribo_reads_target_2, by = "locus_tag", all = TRUE) %>% select(c(-"StartPosition", -"EndPosition", -"Sequence", -"Strand"))
+#Write into excel
+write_xlsx(Merged_ratios, "C:/Users/aagnoli/Desktop/Merged ppGpp 16h.xlsx")
 
 #write list of sequences for the motif search in kplogo
 write_csv(input_data_max, file = "C:/Users/aagnoli/Desktop/RBS_peak_finder_output_2.csv")
